97e4a5f23b5ae2a1a6ec209967f3db37e761b252
Olaide Adebanjo <olaideadebanjo@gmail.com>
2022-01-13 11:57:27 -0800

    Working with Array and composit keys during collection Resource

--- CourseLibrary/CourseLibraryAPI/Controllers/AuthorCollectionsController.cs
+++ CourseLibrary/CourseLibraryAPI/Controllers/AuthorCollectionsController.cs
@@ -1,9 +1,11 @@
 ﻿using AutoMapper;
+using CourseLibraryAPI.Helpers;
 using CourseLibraryAPI.Models;
 using CourseLibraryAPI.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CourseLibraryAPI.Controllers
 {
@@ -21,21 +23,48 @@ namespace CourseLibraryAPI.Controllers
                 throw new ArgumentNullException(nameof(courseLibraryRepository));
             _mapper = mapper ??
                 throw new ArgumentNullException(nameof(mapper));
-
         }
+
+        [HttpGet("({ids})", Name = "GetAuthorCollection")]
+        public IActionResult GetAuthorCollection(
+        [FromRoute]
+        [ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<Guid> ids)
+        {
+            if (ids == null)
+            {
+                return BadRequest();
+            }
+
+            var authorEntities = _courseLibraryRepository.GetAuthors(ids);
+
+            if (ids.Count() != authorEntities.Count())
+            {
+                return NotFound();
+            }
+
+            var authorsToReturn = _mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+
+            return Ok(authorsToReturn);
+        }
+
 
         [HttpPost]
         public ActionResult<IEnumerable<AuthorDto>> CreateAuthorCollection(
             IEnumerable<AuthorForCreationDto> authorCollection)
         {
             var authorEntities = _mapper.Map<IEnumerable<Entities.Author>>(authorCollection);
-            foreach(var author in authorEntities)
+            foreach (var author in authorEntities)
             {
                 _courseLibraryRepository.AddAuthor(author);
             }
+
             _courseLibraryRepository.Save();
 
-            return Ok(); 
+            var authorCollectionToReturn = _mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+            var idsAsString = string.Join(",", authorCollectionToReturn.Select(a => a.Id));
+            return CreatedAtRoute("GetAuthorCollection",
+             new { ids = idsAsString },
+             authorCollectionToReturn);
         }
     }
 }
