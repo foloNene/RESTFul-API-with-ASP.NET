6ef459e837352944c49c2620605c72dbb4d80a83
Olaide Adebanjo <olaideadebanjo@gmail.com>
2022-01-14 13:10:49 -0800

    Working with Validation and Customizing Error Messages

--- CourseLibrary/CourseLibraryAPI/Models/CourseForCreationDto.cs
+++ CourseLibrary/CourseLibraryAPI/Models/CourseForCreationDto.cs
@@ -1,11 +1,29 @@
-﻿using System;
+﻿using CourseLibraryAPI.ValidationAttributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CourseLibraryAPI.Models
 {
-    public class CourseForCreationDto
+    [CourseTitleMustBeDifferentFromDescriptionAttribute(
+        ErrorMessage = "Title must be differnt from description.")]
+    public class CourseForCreationDto //: IValidatableObject
     {
+        [Required(ErrorMessage = "You should fill out a title")]
+        [MaxLength(100, ErrorMessage ="The title shouldn't have more than 1500 characters.")]
         public string Title { get; set; }
+
+        [MaxLength(1500)]
         public string Description { get; set; }
-       
+
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //   if(Title == Description)
+        //    {
+        //        yield return new ValidationResult(
+        //            "The provided description should be different from the title.",
+        //            new[] { "CourseForCreationDto" });
+        //    }
+        //}
     }
 }
