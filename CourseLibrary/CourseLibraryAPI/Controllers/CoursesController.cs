c57abaed53680a253bd07cad1704cfec384e4605
Olaide Adebanjo <olaideadebanjo@gmail.com>
2022-01-15 22:51:23 -0800

    Validating Input when Upserting with PATCH

--- CourseLibrary/CourseLibraryAPI/Controllers/CoursesController.cs
+++ CourseLibrary/CourseLibraryAPI/Controllers/CoursesController.cs
@@ -131,7 +131,12 @@ namespace CourseLibraryAPI.Controllers
             if(courseForAuthorFromRepo == null)
             {
                 var courseDto = new CourseForUpdateDto();
-                patchDocument.ApplyTo(courseDto);
+                patchDocument.ApplyTo(courseDto, ModelState);
+
+                if (!TryValidateModel(courseDto))
+                {
+                    return ValidationProblem(ModelState);
+                }
                 var courseToAdd = _mapper.Map<Entities.Course>(courseDto);
                 courseToAdd.Id = courseId;
 
