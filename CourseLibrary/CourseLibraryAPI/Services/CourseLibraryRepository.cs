a1acadde5cd174ca99d006f67914f12544f68bd0
Olaide Adebanjo <olaideadebanjo@gmail.com>
2022-01-20 13:56:20 -0800

    Sorting Resource Collections

--- CourseLibrary/CourseLibraryAPI/Services/CourseLibraryRepository.cs
+++ CourseLibrary/CourseLibraryAPI/Services/CourseLibraryRepository.cs
@@ -156,6 +156,18 @@ namespace CourseLibraryAPI.Services
                 || a.FirstName.Contains(searchQuery)
                 || a.LastName.Contains(searchQuery));
             }
+
+            if (!string.IsNullOrWhiteSpace(authorsResourceParameters.OrderBy))
+            {
+                if(authorsResourceParameters.OrderBy.ToLowerInvariant() == "name")
+                {
+                    collection = collection.OrderBy(a => a.FirstName).ThenBy(a => a.LastName);
+                }
+
+                collection.ApplySort(authorsResourceParameters.OrderBy, mappingDictionary)
+               
+            }
+
             return PagedList<Author>.Create(collection,
                 authorsResourceParameters.PageNumber,
                 authorsResourceParameters.PageSize);
