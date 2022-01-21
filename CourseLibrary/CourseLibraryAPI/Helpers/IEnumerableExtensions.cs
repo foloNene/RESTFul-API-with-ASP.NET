b025b380933dc8125de1988044ea0deba7349a6c
Olaide Adebanjo <olaideadebanjo@gmail.com>
2022-01-21 11:53:58 -0800

    Creating a Reusable Extension Method to shape DAtA

---
+++ CourseLibrary/CourseLibraryAPI/Helpers/IEnumerableExtensions.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Reflection;
+
+namespace CourseLibraryAPI.Helpers
+{
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<ExpandoObject> ShapeData<TSource>(
+            this IEnumerable<TSource> source,
+            string fields)
+        {
+            if(source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            //create a list to hold our ExpandoObjects
+            var expandoObjectList = new List<ExpandoObject>();
+            // create a list with PropertyInfo objectd on TSource. Reflection is 
+            // expansive, so rather than doing it for each object in the list, we do
+            //it once snd reuse the results. After all, part of the reflection is on the 
+            // type of the object(TSource), not on the instance
+            var propertyInfoList = new List<PropertyInfo>();
+
+            if (string.IsNullOrWhiteSpace(fields))
+            {
+                // all public properties should be in the ExpandoObject
+                var propertyInfos = typeof(TSource)
+                    .GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+                propertyInfoList.AddRange(propertyInfos);
+            }
+            else
+            {
+                //the fireld are sepearated by ",", so we split it.
+                var fieldsAfterSplit = fields.Split(',');
+
+                foreach(var field in fieldsAfterSplit)
+                {
+                    //trim each field, as it might contain leading
+                    // or trailing spacing. can't trim the var in foreach,
+                    //so use another var.
+                    var propertyName = field.Trim();
+
+                    //use reflection to get the property on the source object
+                    // we need to include public and instance, b/c specifying a binding
+                    //flag overwrites the already- existing binding flags.
+
+                    var propertyInfo = typeof(TSource)
+                        .GetProperty(propertyName, BindingFlags.IgnoreCase |
+                        BindingFlags.Public | BindingFlags.Instance);
+
+                    if(propertyInfo == null)
+                    {
+                        throw new Exception($"Property {propertyName} was't found on" +
+                            $"{ typeof(TSource)}");
+                    }
+
+                    //add propertyInfo to lIst
+                    propertyInfoList.Add(propertyInfo);
+                }
+            }
+
+            //run through the source objects
+            foreach(TSource sourceObejct in source)
+            {
+                // create an ExpandoObject that will hold the
+                //selected properties and values
+
+                var dataShapedObject = new ExpandoObject();
+
+               // Get the value of each Property we have to return. for that
+               // we run through the list
+               foreach(var propertyInfo in propertyInfoList)
+                {
+                    //GetValue return the value of the property on the source Object
+                    var propertyValue = propertyInfo.GetValue(sourceObejct);
+
+                    //add the field to the ExpandoObject
+                    ((IDictionary<string, object>)dataShapedObject)
+                        .Add(propertyInfo.Name, propertyValue);
+                }
+
+               //add the ExpandoObject to the List
+               expandoObjectList.Add(dataShapedObject);
+            }
+
+            //return the list
+            return expandoObjectList;
+        }
+    }
+}
